bfb4eae3c081d2b0f14362ca465d1465bc685719
PedroPK <pedropk@gmail.com>
2023-04-18 18:44:15 -0300

    Email Unitary Tests with Jupiter API and Engine

---
+++ cleanArchScholl/src/test/java/cleanArchScholl/commons/EmailTest.java
@@ -0,0 +1,52 @@
+package cleanArchScholl.commons;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EmailTest {
+
+	@Test
+	void invalidNullEmailAddress() {
+		assertThrows(
+			IllegalArgumentException.class, 
+			() -> new Email(null)
+		);
+	}
+	
+	@Test
+	void invalidEmptyEmailAddress() {
+		assertThrows(
+			IllegalArgumentException.class, 
+			() -> new Email("")
+		);
+	}
+	
+	@Test
+	void invalidWithoutAtEmailAddress() {
+		assertThrows(
+				IllegalArgumentException.class, 
+				() -> new Email("myemail.com")
+				);
+	}
+	
+	@Test
+	void invalidWithoutDotEmailAddress() {
+		assertThrows(
+				IllegalArgumentException.class, 
+				() -> new Email("myemail@provider")
+				);
+	}
+	
+	@Test
+	void validEmailAddress() {
+		// Arrange
+		String emailAddress = "myEmail@provider.com";
+		
+		// Act
+		Email email = new Email(emailAddress);
+		
+		assertNotNull(email);
+	}
+
+}
