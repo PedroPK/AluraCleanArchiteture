24a0b8f6abbf20b7aad7e7d2534a11798ff9bc0f
PedroPK <pedropk@gmail.com>
2023-04-21 18:52:04 -0300

    String Utils validation implemented

---
+++ cleanArchScholl/src/test/java/cleanArchScholl/utils/StringUtilsTest.java
@@ -0,0 +1,69 @@
+package cleanArchScholl.utils;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StringUtilsTest {
+
+	@Test
+	void testNotNullString() {
+		// Arrange
+		String nullString = null;
+		
+		// Act
+		boolean response = StringUtils.isStringValid(nullString);
+		
+		// Assert
+		assertFalse(response);
+	}
+	
+	@Test
+	void testNotEmptyString() {
+		// Arrange
+		String emptyString = "";
+		
+		// Act
+		boolean response = StringUtils.isStringValid(emptyString);
+		
+		// Assert
+		assertFalse(response);
+	}
+	
+	@Test
+	void testSpaceString() {
+		// Arrange
+		String spaceString = " ";
+		
+		// Act
+		boolean response = StringUtils.isStringValid(spaceString);
+		
+		// Assert
+		assertFalse(response);
+	}
+	
+	@Test
+	void testDoubleSpaceString() {
+		// Arrange
+		String doubleSpaceString = "  ";
+		
+		// Act
+		boolean response = StringUtils.isStringValid(doubleSpaceString);
+		
+		// Assert
+		assertFalse(response);
+	}
+	
+	@Test
+	void testDotString() {
+		// Arrange
+		String dotString = ".";
+		
+		// Act
+		boolean response = StringUtils.isStringValid(dotString);
+		
+		// Assert
+		assertTrue(response);
+	}
+
+}
