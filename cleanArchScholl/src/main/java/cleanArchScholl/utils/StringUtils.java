5374f1b18bbf92b682782696025a39ab69a428d2
PedroPK <pedropk@gmail.com>
2023-04-22 16:33:11 -0300

    String Utils and its Test classes

--- cleanArchScholl/src/main/java/cleanArchScholl/utils/StringUtils.java
+++ cleanArchScholl/src/main/java/cleanArchScholl/utils/StringUtils.java
@@ -1,15 +1,15 @@
 package cleanArchScholl.utils;
 
 public class StringUtils {
-	
+
 	public static boolean isStringValid(String pString) {
 		boolean result = false;
-		
+
 		if ( pString != null && !pString.isEmpty() && !pString.isBlank()) {
 			result = true;
 		}
-		
+
 		return result;
 	}
-	
+
 }
--- cleanArchScholl/src/test/java/cleanArchScholl/utils/StringUtilsTest.java
+++ cleanArchScholl/src/test/java/cleanArchScholl/utils/StringUtilsTest.java
@@ -10,58 +10,58 @@ class StringUtilsTest {
 	void testNotNullString() {
 		// Arrange
 		String nullString = null;
-		
+
 		// Act
 		boolean response = StringUtils.isStringValid(nullString);
-		
+
 		// Assert
 		assertFalse(response);
 	}
-	
+
 	@Test
 	void testNotEmptyString() {
 		// Arrange
 		String emptyString = "";
-		
+
 		// Act
 		boolean response = StringUtils.isStringValid(emptyString);
-		
+
 		// Assert
 		assertFalse(response);
 	}
-	
+
 	@Test
 	void testSpaceString() {
 		// Arrange
 		String spaceString = " ";
-		
+
 		// Act
 		boolean response = StringUtils.isStringValid(spaceString);
-		
+
 		// Assert
 		assertFalse(response);
 	}
-	
+
 	@Test
 	void testDoubleSpaceString() {
 		// Arrange
 		String doubleSpaceString = "  ";
-		
+
 		// Act
 		boolean response = StringUtils.isStringValid(doubleSpaceString);
-		
+
 		// Assert
 		assertFalse(response);
 	}
-	
+
 	@Test
 	void testDotString() {
 		// Arrange
 		String dotString = ".";
-		
+
 		// Act
 		boolean response = StringUtils.isStringValid(dotString);
-		
+
 		// Assert
 		assertTrue(response);
 	}
